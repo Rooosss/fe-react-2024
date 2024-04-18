e6f4299dfa9ffa0017b67e6d928f536bbdde8bd1
Rooosss <vyshemirskyirostyslav@gmail.com>
2024-04-18 22:16:18 +0300

    Fixed paragraphs and renamed the About component

--- src/components/block/About.component.tsx
+++ src/components/block/About.component.tsx
@@ -8,17 +8,17 @@ export default function About() {
                 <p className="about__text">
                     Hi! My name is Rostyslav Vyshemirskyi and I&#39;m a Junior Frontend Developer. I am already familiar with main Web
                     Technologies like React, HTML, CSS, JavaScript and Git version control system.
-                    <br />
-                    <br />
+                </p>
+                <p className="about__text">
                     This page was page was developed during the course &#39;
                     <a href="https://www.mastersacademy.education/frontend-react-it">Intro to React</a>
                     &#39; from Masters Academy in 2024.
-                    <br />
-                    <br />
+                </p>
+                <p className="about__text">
                     This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own
                     small project for the portfolio.
-                    <br />
-                    <br />
+                </p>
+                <p className="about__text">
                     You can contact me via
                     <a href="//t.me/Rohanskyi"> Telegram</a> and check out my
                     <a href="https://github.com/Rooosss"> GitHub</a>.
