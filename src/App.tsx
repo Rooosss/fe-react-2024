432f2c360d170a2976b96b5fd4e3a9dc31cfa8e7
Rooosss <vyshemirskyirostyslav@gmail.com>
2024-04-29 15:49:54 +0300

    icons were changed to svg, non-working links were removed and hover effects were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import FooterComponent from '@/components/footer/Footer.component.tsx';
-
-import AboutComponent from './components/block/About.component.tsx';
+import AboutComponent from './components/about/About.component.tsx';
+import FooterComponent from './components/footer/Footer.component.tsx';
 import HeaderComponent from './components/header/Header.component.tsx';
 
 import styles from './App.module.css';
--- src/components/footer/Footer.component.tsx
+++ src/components/footer/Footer.component.tsx
@@ -1,10 +1,6 @@
-import FacebookIcon from '@/image/fb-icon.png';
-import InstagramIcon from '@/image/insta-icon.png';
-import LinkedinIcon from '@/image/linkedin-icon.png';
-
 import styles from './Footer.module.css';
 
-const FacebookLink = '#';
+const MOCLink = 'https://www.mastersacademy.education/frontend-for-beginners-it';
 const LinkedinLink = 'https://www.linkedin.com/in/rostyslav-vyshemirskyi-b33bbb1b4/';
 const InstagramLink = 'https://www.instagram.com/rostik_v_/';
 
@@ -14,19 +10,24 @@ export default function Footer() {
             <div className={styles.footer__container}>
                 <span className={styles.footer__line}></span>
                 <div className={styles.footer__social_link}>
-                    <a className={styles.footer__social_item} href={FacebookLink}>
-                        <img className={styles.footer__social_image} src={FacebookIcon} width="25px" height="25px" alt="facebook icon" />
+                    <a className={styles.footer__social_linkedin} href={LinkedinLink}>
+                        <svg width="35" height="35" viewBox="0 0 35 35" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17.5 0C7.83649 0 0 7.83649 0 17.5C0 27.1635 7.83649 35 17.5 35C27.1635 35 35 27.1635 35 17.5C35 7.83649 27.1635 0 17.5 0ZM12.4147 26.4551H8.15266V13.6326H12.4147V26.4551ZM10.2838 11.8817H10.256C8.82584 11.8817 7.90085 10.8972 7.90085 9.66671C7.90085 8.40847 8.85414 7.45117 10.3121 7.45117C11.7701 7.45117 12.6673 8.40847 12.6951 9.66671C12.6951 10.8972 11.7701 11.8817 10.2838 11.8817ZM27.7827 26.4551H23.5212V19.5954C23.5212 17.8714 22.9041 16.6957 21.362 16.6957C20.1847 16.6957 19.4835 17.4888 19.1753 18.2544C19.0627 18.5283 19.0351 18.9112 19.0351 19.2944V26.4551H14.7734C14.7734 26.4551 14.8292 14.8356 14.7734 13.6326H19.0351V15.4482C19.6015 14.5744 20.6149 13.3317 22.8761 13.3317C25.6802 13.3317 27.7827 15.1643 27.7827 19.1027V26.4551Z" />
+                        </svg>
                     </a>
-                    <a className={styles.footer__social_item} href={LinkedinLink}>
-                        <img className={styles.footer__social_image} src={LinkedinIcon} width="25px" height="25px" alt="Linkedin icon" />
-                    </a>
-                    <a className={styles.footer__social_item} href={InstagramLink}>
-                        <img className={styles.footer__social_image} src={InstagramIcon} width="25px" height="25px" alt="Instagram icon" />
+                    <a className={styles.footer__social_instagram} href={InstagramLink}>
+                        <svg width="35" height="35" viewBox="0 0 35 35" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M17.5 35C27.165 35 35 27.165 35 17.5C35 7.83502 27.165 0 17.5 0C7.83502 0 0 7.83502 0 17.5C0 27.165 7.83502 35 17.5 35ZM14.0047 17.5C14.0047 15.58 15.5646 14.005 17.4995 14.005C19.4194 14.005 20.9944 15.58 20.9944 17.5C20.9944 19.42 19.4194 20.995 17.4995 20.995C15.5796 20.995 14.0047 19.42 14.0047 17.5ZM27.9241 13.165C27.9991 14.62 27.9991 16.045 27.9991 17.5C27.9991 17.6821 27.9993 17.8639 27.9995 18.0456C28.0012 19.3154 28.0028 20.5752 27.9241 21.835C27.8491 23.515 27.4591 25 26.2291 26.23C25.0142 27.46 23.5143 27.85 21.8343 27.925C20.3794 28 18.9545 28 17.4995 28C16.0446 28 14.6197 28 13.1647 27.925C11.4848 27.85 9.99987 27.46 8.76992 26.23C7.53998 25.015 7.14999 23.515 7.075 21.835C7 20.38 7 18.955 7 17.5C7 16.045 7 14.62 7.075 13.165C7.14999 11.485 7.53998 10 8.76992 8.77C9.98487 7.54 11.4848 7.15 13.1647 7.075C14.6197 7 16.0446 7 17.4995 7C18.9545 7 20.3794 7 21.8343 7.075C23.5143 7.15 24.9992 7.54 26.2291 8.77C27.4591 9.985 27.8491 11.485 27.9241 13.165ZM12.1148 17.5C12.1148 20.485 14.5147 22.885 17.4995 22.885C20.4844 22.885 22.8843 20.485 22.8843 17.5C22.8843 14.515 20.4844 12.115 17.4995 12.115C14.5147 12.115 12.1148 14.515 12.1148 17.5ZM21.8493 11.89C21.8493 12.595 22.4193 13.15 23.1093 13.15L23.1243 13.135C23.2886 13.1388 23.4519 13.1088 23.6041 13.0469C23.7564 12.9849 23.8942 12.8924 24.0092 12.775C24.1266 12.66 24.2192 12.5221 24.2811 12.3699C24.343 12.2176 24.373 12.0543 24.3692 11.89C24.3692 11.185 23.8142 10.63 23.1093 10.63C22.4043 10.63 21.8493 11.185 21.8493 11.89Z"
+                            />
+                        </svg>
                     </a>
                 </div>
                 <p className={styles.footer__description}>
                     Made with 💗 on course{' '}
-                    <a className={styles.footer__description_link} href="https://www.mastersacademy.education/frontend-for-beginners-it">
+                    <a className={styles.footer__description_link} href={MOCLink}>
                         &#39;Intro to React&#39; from Masters Academy in 2024
                     </a>{' '}
                     by Rostyslav
