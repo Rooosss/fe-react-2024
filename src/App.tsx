a7cd23411bc230da164cd1f455ac243b3f68dfe5
Rooosss <vyshemirskyirostyslav@gmail.com>
2024-05-03 01:15:55 +0300

    The ProductCard and ProducstList components have been added, which show cards from the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import AboutComponent from './components/about/About.component.tsx';
-import FooterComponent from './components/footer/Footer.component.tsx';
-import HeaderComponent from './components/header/Header.component.tsx';
+import { useState } from 'react';
+import React from 'react';
+
+import About from './components/about/About.component.tsx';
+import Footer from './components/footer/Footer.component.tsx';
+import Header from './components/header/Header.component.tsx';
+import ProductsList from './components/product/ProductsList.component.tsx';
 
 import styles from './App.module.css';
 
-export default function App() {
+function App() {
+    const apiUrl = 'https://ma-backend-api.mocintra.com/api/v1/products?limit=8&offset=0';
+    const [isShowPage, setPage] = useState<string>('About');
+
+    const handleShowAbout = () => {
+        setPage('About');
+    };
+
+    const handleShowProducts = () => {
+        setPage('ProductsList');
+    };
+
     return (
         <>
-            <HeaderComponent />
+            <Header onShowAbout={handleShowAbout} onShowProducts={handleShowProducts} />
             <main className={styles.home}>
-                <div className={styles.container}>
-                    <AboutComponent />
-                </div>
+                <div className={styles.container}>{isShowPage === 'About' ? <About /> : <ProductsList apiUrl={apiUrl} />}</div>
             </main>
-            <FooterComponent />
+            <Footer />
         </>
     );
 }
+
+export default App;
